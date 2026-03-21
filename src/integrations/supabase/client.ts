a5abba63f6b3e4cd0974c81a40ef5d8069feab34
martvitalis1-ai <martvitalis1@gmail.com>
2026-03-20 20:15:08 -0400

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // METEMOS LOS DATOS A LA FUERZA PARA SALTARNOS EL ERROR
 const supabaseUrl = "https://qfdrmyuuswiubsppyjrt.supabase.co";
-const supabaseAnonKey = "COPIA_AQUI_TU_LLAVE_ANOM_LARGA_QUE_EMPIEZA_CON_EYJ";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmZHJteXV1c3dpdWJzcHB5anJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MDU1NDEsImV4cCI6MjA4NjQ4MTU0MX0.u3afSc4T--SnAniJOcjiUdVodP1p1aFof3FjqtZNlqY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
