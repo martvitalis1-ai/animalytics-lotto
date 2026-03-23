303ee38c3352f970eb93413dc93ea78d99e002e3
martvitalis1-ai <martvitalis1@gmail.com>
2026-03-23 09:58:00 -0400

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,6 +10,5 @@ export const LOTTERIES = [
 ];
 
 export const getDrawTimesForLottery = (lotteryId: string): string[] => {
-  const common = ["08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM", "06:00 PM", "07:00 PM"];
-  return common;
+  return ["08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM", "06:00 PM", "07:00 PM"];
 };
