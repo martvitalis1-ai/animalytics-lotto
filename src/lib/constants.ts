bb095f86e4c4973b27420450d552c178aedaaadc
martvitalis1-ai <martvitalis1@gmail.com>
2026-03-23 21:29:29 -0400

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,8 +10,7 @@ export const LOTTERIES = [
 ];
 
 export const getDrawTimesForLottery = (id: string) => [
-  "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", 
-  "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM", "06:00 PM", "07:00 PM"
+  "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM", "06:00 PM", "07:00 PM"
 ];
 
 export const formatResultNumber = (n: string) => {
